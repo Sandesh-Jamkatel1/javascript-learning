2f95cc6e4fb346fe6ed503d2cf03dbcbe877e371
Sandesh-Jamkatel1 <sandeshjamkatel68@gmail.com>
2024-09-04 14:02:21 +0000

    Commit on dataTypesConversion

---
+++ 01_basics/03_dataTypesConversion.js
@@ -0,0 +1,22 @@
+// let score = "sandesh";
+// console.log(typeof score)
+// console.log(typeof (score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// "33" =>33
+// "33abc" => NaN
+// true =>1
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// in boolean 1=>true; 0 ==>true
+// " "=>false
+let Num1 = 33
+let Num2 = 55
+let stringNumber = String(Num1)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber+Num2)
+stringNumber = Number(Num1)
+console.log(stringNumber+Num2)
